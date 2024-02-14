b5d64097d33620f21ee656f57699e96c31eaefbc
Charmzyy <victor.githaiga@strathmore.edu>
2024-02-14 19:53:14 +0300

    fix:render files

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
-echo "Running composer"
+echo 'Running composer'
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
-
-echo "generating application key..."
-php artisan key:generate --show
-
-echo "Caching config..."
+ 
+echo 'Caching config...'
 php artisan config:cache
-
-echo "Caching routes..."
+ 
+echo 'Caching routes...'
 php artisan route:cache
-
-echo "Running migrations..."
+ 
+echo 'Running migrations...'
 php artisan migrate --force
