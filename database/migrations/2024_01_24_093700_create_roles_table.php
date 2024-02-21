836519c8a3a28ae1abcd8c3f53822cef1f81dbc9
Charmzyy <victor.githaiga@strathmore.edu>
2024-02-21 10:08:47 +0300

    chore:create service,invoices,departments,migrations

--- database/migrations/2024_01_24_093700_create_roles_table.php
+++ database/migrations/2024_01_24_093700_create_roles_table.php
@@ -11,9 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('roles', function (Blueprint $table) {
+        Schema::create('departments', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->longtext('description');
             $table->timestamps();
         });
     }
