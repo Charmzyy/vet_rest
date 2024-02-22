4285bd7a5118823dc4bc12cbfded90b45b30dbf4
Charmzyy <victor.githaiga@strathmore.edu>
2024-02-22 08:57:21 +0300

    fix nullable on dept_id

--- database/migrations/2024_02_21_065616_add_field_user_table.php
+++ database/migrations/2024_02_21_065616_add_field_user_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->unsignedBigInteger('dept_id')->after('phone');
+            $table->unsignedBigInteger('dept_id')->after('phone')->nullable();
             $table->foreign('dept_id')->references('id')->on('departments');
         });
     }
